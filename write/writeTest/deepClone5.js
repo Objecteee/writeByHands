781404d192cf833021b453aaa7da8399b59545f2
Objecteee <1311022959@qq.com>
2026-01-07 14:21:24 +0800

    复习了deepClone

---
+++ write/writeTest/deepClone5.js
@@ -0,0 +1,25 @@
+const deepClone=(source,hash=new WeakMap())=>{
+    if(source!=='object'||source===null) return source;
+    if(hash.has(source)) return hash.get(source);
+    if(source instanceof Date) return new Date(source);
+    if(source instanceof RegExp) return new RegExp(source);
+    if(source instanceof Map){
+        const cloned=new Map();
+        hash.set(source,cloned);
+        source.forEach((value,key)=>{
+        cloned.set(deepClone(key, hash), deepClone(value, hash));
+        })
+    }
+    if(source instanceof Set){
+        const cloned=new Set();
+        hash.set(source,cloned);
+        source.forEach((key)=>{
+        cloned.add(deepClone(key,hash));
+        })
+    }
+    cloned=Array.isArray(source)?[]:Object.create(Object.getPrototypeOf(source));
+    hash.set(source,cloned);
+    Reflect.ownKeys(source).forEach((key)=>{
+        cloned[key]=deepClone(source[key],hash);
+    })
+}
